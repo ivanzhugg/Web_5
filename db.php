bc6a6bcd4e6056f267e7790f02e2381907ad9053
ivanzhugg <78428291+ivanzhugg@users.noreply.github.com>
2025-05-26 21:58:47 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,22 +1,13 @@
 <?php
-
 $servername = "localhost";
 $username = "root";
 $password = "kraga228";
 $dbname = "php_home3";
 
-
-
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 
-if (!$conn)
-{
-    die("Error". mysqli_connect_error());
+if (!$conn) {
+    error_log("Connection failed: " . mysqli_connect_error());
+    die("Ошибка подключения к базе данных. Пожалуйста, попробуйте позже.");
 }
-
-else 
-{
-    echo "GREAT";
-}
-
 ?>
